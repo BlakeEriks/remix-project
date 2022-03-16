6ab72c01b5cc1f365d1a066a9e929f7fe2681107
Blake Eriks <eblake98@vt.edu>
2022-03-16 10:52:59 -0700

    Remove random 4

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,7 +2,7 @@ import { json, LoaderFunction, useLoaderData } from "remix"
 import invariant from "tiny-invariant"
 import { getPost } from "~/post"
 
-export const loader: LoaderFunction = async ({params}) => {4
+export const loader: LoaderFunction = async ({params}) => {
   invariant(params.slug, "expected params.slug")
   return json(await getPost(params.slug))
 }
